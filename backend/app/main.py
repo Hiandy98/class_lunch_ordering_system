3f6ac5506e159ffba573983e2aceaec718ca383c
Hiandy98 <mangoandy98@hotmail.com>
2026-03-13 20:16:08 +0800

    feat: add logging system

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,23 @@
+import logging
+from utils.logger import LoggerParameter, Logger
+
+
 def main():
-    print("Hello from v0-0!")
+    log_params = LoggerParameter(
+        AI_instructions="""
+        [PROJECT CONTEXT]
+        Name: class_lunch_ordering_systeam
+        Goal: 一個班級的訂餐系統 在此輸出後端運行日誌
+        """,
+        logging_level="DEBUG",
+        do_session_log= True,
+        do_console_log= True,
+        do_master_log = True,
+    )
+
+    Logger.init(log_params)
+    
+    logging.info("Hello from class-lunch-ordering-system V0.0-Beta")
 
 
 if __name__ == "__main__":
