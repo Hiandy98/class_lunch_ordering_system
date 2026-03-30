ccf7b1590f232a11188ddc02e38f952e89e4c1da
Hiandy98 <mangoandy98@hotmail.com>
2026-03-30 21:36:51 +0800

    feat(api): add user and order management api

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from app.api.v1.stores import store_router
 from app.api.v1.orders import order_router
 from app.api.v1.auth import auth_router
+from app.api.v1.users import users_router
 
 api_v1_router = APIRouter(prefix="/api/v1")
 
 api_v1_router.include_router(store_router, prefix="/store", tags=["Stores"])
 api_v1_router.include_router(order_router, prefix="/order", tags=["Orders"])
 api_v1_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
+api_v1_router.include_router(users_router, prefix="/user", tags=["User"])
