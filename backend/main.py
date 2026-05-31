8fb97dc386c0fd415eeb769ed47d74c6075191b4
Hiandy98 <mangoandy98@hotmail.com>
2026-05-31 20:21:54 +0800

    fix(cors): explicitly specify dev and production origins to comply with credentials

--- backend/main.py
+++ backend/main.py
@@ -48,11 +48,12 @@ origins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5500",
+    "http://localhost:4173"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
